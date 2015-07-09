648e2e8364b17f3338ac1d0fcc53c2052ba635d2
yang <yang@yangdeMac-mini.local>
2015-07-09 16:53:37 +0800

    Initial Commit

---
+++ YesView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YesView
+//
+//  Created by yang on 15/7/9.
+//  Copyright (c) 2015年 yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
